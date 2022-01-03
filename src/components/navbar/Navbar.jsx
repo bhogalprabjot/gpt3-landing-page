a70d150f468d5bd617e08dd9c82f356ceb91dbb7
Prabjot Singh Bhogal <bhogalprabjot1@gmail.com>
2022-01-03 19:25:03 +0530

    updates

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './navbar.css';
+import { RiMenu3Line, RiCloseLin }
+    from 'react-icons';
+import logo from '../../assets/GPT-3.svg'
+
+
+const Navbar = () => {
+    return (
+        <div className="gpt3__navbar">
+            <div className="gpt3__navbar-links">
+                <div className="gpt3__navbar-links_logo">
+                    <img src={logo} alt="logo" />
+                </div>
+                <div className="gpt3__navbar-links_container">
+                    <p><a href="#home">Home</a></p>
+                    <p><a href="#whatGPT3">What is GPT?</a></p>
+                    <p><a href="#possibility">Open AI</a></p>
+                    <p><a href="#features">Case Studies</a></p>
+                    <p><a href="#blog">Library</a></p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
